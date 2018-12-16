8cccb991ad69f7f41fcbec5bfb1cd540df89ca9a
dhisss <38402253+dhisss@users.noreply.github.com>
2018-12-16 16:07:03 +0700

    Add files via upload

---
+++ PowerMethod.R
@@ -0,0 +1,16 @@
+library(matlib)
+
+A = rbind(c(4, -5),
+          c(2, -3))
+A
+
+powerMethod(A)
+
+
+
+B = rbind(c(0, 11 , -5),
+          c(-2 , 17 , -7), 
+          c(-4 , 26 , -10))
+B
+
+powerMethod(B)
